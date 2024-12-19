ac1bf3463cf4b35b3d91198daf3f3f1708bdd0e4
Csanády Mátyás <matyas.csanady@in-ex.hu>
2024-12-19 11:02:18 +0100

    Hello world

---
+++ hello-rust/src/main.rs
@@ -0,0 +1,12 @@
+// Comments are like these
+// Like in C# the entry point is the main function
+fn main()
+{
+    println!("Hello, world!");
+    test();
+}
+
+fn test()
+{
+    println!("This is printed from the test function!");
+}
